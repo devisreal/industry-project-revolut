386423f8d67b752039c22a7b1c1a63f538191f6b
Richard Oppong-Konadu <richardok@gmail.com>
2025-03-28 13:52:29 +0000

    create week page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import LandingPage from "./screens/LandingPage/LandingPage";
 import Screen from "./components/Screen/Screen";
 import ExploreProfileScreen from "./screens/ExploreProfileScreen/ExploreProfileScreen";
+import WeekByWeek from "./screens/WeekByWeek/WeekByWeek";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         {/* Explorer Profile Screen */}
         <Route path="/explore-screen" element={<ExploreProfileScreen />} />
+        <Route path="/weekly" element={<WeekByWeek />} />
       </Routes>
     </BrowserRouter>
   );
