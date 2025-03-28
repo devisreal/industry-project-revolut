45ac71206b97d94bfbef19ef712cfb704841b1ab
Richard Oppong-Konadu <richardok@gmail.com>
2025-03-28 10:48:14 +0000

    add carousel component

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,19 @@
 import "./LandingPage.scss";
 import homepageImage from "../../assets/images/homescreen.png";
+import carouselPng from "../../assets/images/carousel.png";
 
 export default function LandingPage() {
   return (
     <div className="landing-page">
       <img className="image__top" src={homepageImage} alt="" />
       <div className="carousel">
-        <p className="test">Test wrapper</p>
+        <div className="body__wrapper">
+          <h2 className="title">Your Revolut Recap</h2>
+          <p className="body">
+            Track your progess, and get smarter with your spending
+          </p>
+        </div>
+        <img className="carousel__image" src={carouselPng} alt="" />
       </div>
       {/* <img className="image__bottom" src={screenshot2} alt="" /> */}
     </div>
