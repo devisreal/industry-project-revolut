e2c0e4281dfa9e5d98a6766ab34f44ab0b71ee21
Israel Akinoso <akinosoissy@gmail.com>
2025-03-28 13:59:04 +0000

    Subscription Audit Screen complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./screens/LandingPage/LandingPage";
 import Screen from "./components/Screen/Screen";
 import ExploreProfileScreen from "./screens/ExploreProfileScreen/ExploreProfileScreen";
 import SpendingCategory from "./screens/SpendingCategory/SpendingCategory";
+import SubscriptionAuditScreen from "./screens/SubscriptionAuditScreen/SubscriptionAuditScreen";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         {/* Explorer Profile Screen */}
         <Route path="/explore-screen" element={<ExploreProfileScreen />} />
         <Route path="/spending-category" element={<SpendingCategory />} />
+        <Route path="/sub-audit" element={<SubscriptionAuditScreen />} />
       </Routes>
     </BrowserRouter>
   );
